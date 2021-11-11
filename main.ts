57f98966bc576a3f3ed82e12b4a102ddd5e91f57
jafadgex <92938346+jafadgex@users.noreply.github.com>
2021-11-11 11:14:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,28 @@ let Answer = 0
 input.onGesture(Gesture.Shake, function () {
     Answer = randint(0, 7)
     if (Answer == 0) {
+        soundExpression.sad.playUntilDone()
         basic.showString("No way!")
     } else if (Answer == 1) {
+        soundExpression.mysterious.playUntilDone()
         basic.showString("Probably")
     } else if (Answer == 2) {
+        soundExpression.slide.playUntilDone()
         basic.showString("Unlikely")
     } else if (Answer == 3) {
+        soundExpression.giggle.playUntilDone()
         basic.showString("Definitely!")
     } else if (Answer == 4) {
+        soundExpression.giggle.playUntilDone()
         basic.showString("Not sure")
     } else if (Answer == 5) {
+        soundExpression.yawn.playUntilDone()
         basic.showString("Maybe")
     } else if (Answer == 6) {
+        soundExpression.twinkle.playUntilDone()
         basic.showString("I don't think so")
     } else if (Answer == 7) {
+        soundExpression.happy.playUntilDone()
         basic.showString("No doubt about it!")
     } else {
     	
